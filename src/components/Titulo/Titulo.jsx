f4288499a4e9ae40b1dd255e3e00a241a5c6b9e8
Nicolas Caretta <nicocaretta_20@hotmail.com>
2025-04-12 11:19:19 -0300

    titulo

---
+++ src/components/Titulo/Titulo.jsx
@@ -0,0 +1,7 @@
+import styles from './Titulo.module.css';
+
+const Titulo = ({ texto }) => {
+    return <h1 className={styles.titulo}>{texto}</h1>;
+};
+
+export default Titulo;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import Titulo from '../../components/Titulo/Titulo';
+
+const Home = () => {
+    return (
+        <div>
+            <Titulo texto="Gestor de Películas y Series" />
+        </div>
+    );
+};
+
+export default Home;
