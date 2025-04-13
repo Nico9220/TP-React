2c91f5dc366540724657cda332b137ac6b59fe5f
abialmiron <abialmiron@gmail.com>
2025-04-13 19:30:32 -0300

    se comenzo con localstorage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,40 @@
-import { useState } from 'react';
+import { useState,useEffect  } from 'react';
 import Titulo from '../../components/Titulo/Titulo';
 import FormularioItem from '../../components/FormularioItem/FormularioItem';
 import ListaItems from '../../components/ListaItems/ListaItems';
 import Peliculas from '../../components/Peliculas/Peliculas';
+import styles from './Home.module.css';
+
+const LOCAL_STORAGE_POR_VER_KEY = 'peliculasPorVer';
+const LOCAL_STORAGE_VISTAS_KEY = 'peliculasVistas';
 
 const Home = () => {
-  const [porVer, setPorVer] = useState([]);
-  const [vistas, setVistas] = useState([]);
+  const [porVer, setPorVer] = useState(() => {
+    const storedPorVer = localStorage.getItem(LOCAL_STORAGE_POR_VER_KEY);
+    return storedPorVer ? JSON.parse(storedPorVer) : [];
+  });
+  const [vistas, setVistas] = useState(() => {
+    const storedVistas = localStorage.getItem(LOCAL_STORAGE_VISTAS_KEY);
+    return storedVistas ? JSON.parse(storedVistas) : [];
+  });
   const [mostrarFormulario, setMostrarFormulario] = useState(false);
 
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_POR_VER_KEY, JSON.stringify(porVer));
+  }, [porVer]);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_VISTAS_KEY, JSON.stringify(vistas));
+  }, [vistas]);
+
   const handleAgregarPorVer = (nuevoItem) => {
-    setPorVer([...porVer, { ...nuevoItem, visto: false }]); 
-    setMostrarFormulario(false); 
+    setPorVer([...porVer, { ...nuevoItem, visto: false }]);
+    setMostrarFormulario(false);
   };
 
   const handleAgregarVista = (nuevoItem) => {
-    setVistas([...vistas, { ...nuevoItem, visto: true }]); 
-    setMostrarFormulario(false); 
+    setVistas([...vistas, { ...nuevoItem, visto: true }]);
+    setMostrarFormulario(false);
   };
 
   const handleMostrarFormulario = () => {
