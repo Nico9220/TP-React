c4eb704dc266dfc05210964dbee3abce6efae22b
abialmiron <abialmiron@gmail.com>
2025-04-17 11:36:38 -0300

    intentando arreglar el error que le sale a los chicos

--- src/components/ListaItems/ListaItems.jsx
+++ src/components/ListaItems/ListaItems.jsx
@@ -2,14 +2,9 @@ import Item from '../ListaItems/Item/Item';
 import styles from './ListaItems.module.css';
 
 const ListaItems = ({ titulo, items, onMarcarVista }) => {
-    let pelis = localStorage.getItem("pelis");
     if (items.length === 0) {
-        if (pelis.length === 0){
-            return <p>No hay {titulo.toLowerCase()}.</p>;
-        } else {
-            //items.push(pelis);
+        return <p>No hay {titulo.toLowerCase()}.</p>;
         };
-    }
 
 return (
     <div className={styles.lista}>
